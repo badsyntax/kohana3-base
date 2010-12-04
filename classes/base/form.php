a5bba7739e5d97753f18f5b4086f20d12f2935ff
Richard Willis <willis.rh@gmail.com>
2010-12-04 21:47:57 +0000

    moved form helper methods into base class

--- classes/base/form.php
+++ classes/base/form.php
@@ -1,3 +1,42 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-class Base_Form extends Kohana_Form { }
+abstract class Base_Form extends Kohana_Form {
+
+        private static function attributes($name, & $attributes = NULL, $errors = NULL)
+        {
+                // set the id attribute
+                ! isset($attributes['id']) AND $attributes['id'] = $name;
+
+                // set the error classname
+                isset($errors[$name]) AND $attributes['class'] = trim( (string) @$attributes['class'].' error-field');
+        }
+
+        public static function input($name, $value = NULL, array $attributes = NULL, array $errors = NULL)
+        {
+                static::attributes($name, $attributes, $errors);
+
+                return parent::input($name, $value, $attributes);
+        }
+
+        public static function select($name, array $options = NULL, $selected = NULL, array $attributes = NULL, array $errors = NULL)
+        {
+                static::attributes($name, $attributes, $errors);
+
+                return parent::select($name, $options, $selected, $attributes);
+        }
+
+        public static function password($name, $value = NULL, array $attributes = NULL, array $errors = NULL)
+        {
+                static::attributes($name, $attributes, $errors);
+
+                return parent::password($name, $value, $attributes);
+        }
+
+        public static function label($input, $text = NULL, array $attributes = NULL, array $errors = NULL)
+        {
+                isset($errors[$input]) AND $text .= View::factory('admin/messages/label_error')->bind('error', $errors[$input]);
+
+                return parent::label($input, $text, $attributes);
+        }
+
+} // End Base_Form 
