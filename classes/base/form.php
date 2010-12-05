481ff387fb684d039368f97f671fdacb95fd4d64
Richard Willis <willis.rh@gmail.com>
2010-12-05 14:53:03 +0000

    added textarea helper

--- classes/base/form.php
+++ classes/base/form.php
@@ -2,41 +2,49 @@
 
 abstract class Base_Form extends Kohana_Form {
 
-        private static function attributes($name, & $attributes = NULL, $errors = NULL)
-        {
-                // set the id attribute
-                ! isset($attributes['id']) AND $attributes['id'] = $name;
+	private static function attributes($name, & $attributes = NULL, $errors = NULL)
+	{
+		// Set the id attribute
+		! isset($attributes['id']) AND $attributes['id'] = $name;
 
-                // set the error classname
-                isset($errors[$name]) AND $attributes['class'] = trim( (string) @$attributes['class'].' error-field');
-        }
+		// Set the error classname
+		isset($errors[$name]) AND $attributes['class'] = trim( (string) @$attributes['class'].' error-field');
+	}
 
-        public static function input($name, $value = NULL, array $attributes = NULL, array $errors = NULL)
-        {
-                static::attributes($name, $attributes, $errors);
+	public static function input($name, $value = NULL, array $attributes = NULL, array $errors = NULL)
+	{
+		static::attributes($name, $attributes, $errors);
 
-                return parent::input($name, $value, $attributes);
-        }
+		return parent::input($name, $value, $attributes);
+	}
 
-        public static function select($name, array $options = NULL, $selected = NULL, array $attributes = NULL, array $errors = NULL)
-        {
-                static::attributes($name, $attributes, $errors);
+	public static function select($name, array $options = NULL, $selected = NULL, array $attributes = NULL, array $errors = NULL)
+	{
+		static::attributes($name, $attributes, $errors);
 
-                return parent::select($name, $options, $selected, $attributes);
-        }
+		return parent::select($name, $options, $selected, $attributes);
+	}
 
-        public static function password($name, $value = NULL, array $attributes = NULL, array $errors = NULL)
-        {
-                static::attributes($name, $attributes, $errors);
+	public static function password($name, $value = NULL, array $attributes = NULL, array $errors = NULL)
+	{
+		static::attributes($name, $attributes, $errors);
 
-                return parent::password($name, $value, $attributes);
-        }
+		return parent::password($name, $value, $attributes);
+	}
 
-        public static function label($input, $text = NULL, array $attributes = NULL, array $errors = NULL)
-        {
-                isset($errors[$input]) AND $text .= View::factory('admin/messages/label_error')->bind('error', $errors[$input]);
+	public static function textarea($name, $body = '', array $attributes = NULL, $double_encode = TRUE, array $errors = NULL)
+	{
+		static::attributes($name, $attributes, $errors);
 
-                return parent::label($input, $text, $attributes);
-        }
+		return parent::textarea($name, $body, $attributes, $double_encode);
+	}
+	
+	public static function label($input, $text = NULL, array $attributes = NULL, array $errors = NULL)
+	{
+		// Use the label_error view to append an error message to the label
+		isset($errors[$input]) AND $text .= View::factory('admin/messages/label_error')->bind('error', $errors[$input]);
+
+		return parent::label($input, $text, $attributes);
+	}
 
 } // End Base_Form 
