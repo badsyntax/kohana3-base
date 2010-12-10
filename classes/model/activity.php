998492fdc4773511d93ea1ad2422e99eb0c12cc9
Richard Willis <willis.rh@gmail.com>
2010-12-10 21:51:06 +0000

    added activity models

---
+++ classes/model/activity.php
@@ -0,0 +1,4 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Model_Activity extends Model_Base_Activity { }
+
---
+++ classes/model/base/activity.php
@@ -0,0 +1,9 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/*
+ * Base activity model
+ */
+class Model_Base_Activity extends ORM {
+
+	//public $_table_name = 'activity';	
+
+} // End Model_Base_Activity
