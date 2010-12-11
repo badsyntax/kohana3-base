ba7c035cbf61b5ca362c9a7b9f4259248ef3eb23
Richard Willis <willis.rh@gmail.com>
2010-12-11 12:48:19 +0000

    added user relationship definition

--- classes/model/activity.php
+++ classes/model/activity.php
@@ -1,4 +1,6 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-class Model_Activity extends Model_Base_Activity { }
-
+class Model_Activity extends Model_Base_Activity { 
+	
+	protected $_belongs_to = array('user' => array());
+}
