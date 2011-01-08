8678da25494c9780d6e031a4ec6e24f72070bb65
Richard Willis <willis.rh@gmail.com>
2011-01-08 01:04:24 +0000

    added image helper functions

--- classes/model/base/asset/size.php
+++ classes/model/base/asset/size.php
@@ -9,4 +9,60 @@ class Model_Base_Asset_size extends Model_Base {
 	protected $_belongs_to = array(
 		'asset' => array('model' => 'asset', 'foreign_key' => 'asset_id'),
 	);	
+	
+	public function resize($path, $width = NULL, $height = NULL, $crop = NULL)
+	{
+		$file = $this->path(TRUE);
+		
+		if (file_exists($file))
+		{			
+			Asset::resize($file, $path, $width, $height, $crop);
+		}
+	}
+	
+	public function rotate($degrees = 90)
+	{
+		$file = $this->path(TRUE);
+		
+		if (file_exists($file))
+		{
+			Asset::rotate($file, $degrees);
+		}
+	}
+	
+	public function sharpen($amount = 20)
+	{
+		$file = $this->path(TRUE);
+		
+		if (file_exists($file))
+		{
+			Asset::rotate($file, $amount);
+		}
+	}
+	
+	public function flip_horizontal()
+	{
+		$file = $this->path(TRUE);
+		
+		if (file_exists($file))
+		{
+			Asset::flip_horizontal($file);
+		}
+	}
+	public function flip_vertical()
+	{
+		$file = $this->path(TRUE);
+		
+		if (file_exists($file))
+		{
+			Asset::flip_vertical($file);
+		}
+	}
+	
+	public function path($full = FALSE)
+	{
+		$path = Kohana::config('admin/asset.upload_path').'/resized/'.$this->filename;
+		
+		return Asset::path($this, $full);
+	}
 }
